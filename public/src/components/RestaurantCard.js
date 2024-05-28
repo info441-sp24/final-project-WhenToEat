92c14517344a3077c7fea9938bf53de3de9c5ee3
Quang Nguyen <simplyquang@gmail.com>
2024-05-28 03:08:12 -0700

    Got yelp API to work

--- public/src/components/RestaurantCard.js
+++ public/src/components/RestaurantCard.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import '../styles/RestaurantCard.css';
 
-
 const RestaurantCard = ({ restaurant }) => {
     return (
-        <div className="restaurant-card">
+        <a href={restaurant.url} target="_blank" rel="noopener noreferrer" className="restaurant-card">
+            <img src={restaurant.image_url} alt={restaurant.name} className="restaurant-image" />
             <h3>{restaurant.name}</h3>
-            <p>{restaurant.address}</p>
-            <p>{restaurant.cuisine}</p>
-            <p>{restaurant.price_range}</p>
-            <p>{restaurant.rating} Stars</p>
-        </div>
+            <p>{restaurant.location.address1}, {restaurant.location.city}</p>
+            <p className="rating">Rating: {restaurant.rating}</p>
+            <p>Price: {restaurant.price}</p>
+        </a>
     );
-}
+};
 
 export default RestaurantCard;
+
