4a3396537a55076fe5217cb454bfeed30338d319
kaylaxdoan <kaydoan@uw.edu>
2024-05-29 01:29:55 -0700

    fix history

--- public/src/components/Profile.js
+++ public/src/components/Profile.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import RestaurantCard from './RestaurantCard';
 
 const Profile = () => {
   const [user, setUser] = useState(null);
-  const [userError, setUserError] = useState('')
+  const [userError, setUserError] = useState('');
+  const [restaurants, setRestaurants] = useState([]);
   // const [isEditing, setIsEditing] = useState(false);
   // const [editedName, setEditedName] = useState("");
 
@@ -11,13 +13,15 @@ const Profile = () => {
     const urlParams = new URLSearchParams(window.location.hash.split('?')[1]);
     const username = urlParams.get('user');
 
-    const fetchUser = async () => {
+    const fetchUserData = async () => {
       try {
         if (username !== null) {
           let response = await axios.get(`/api/userInfo?username=${username.current.value}`);
           if (response.data.status === "success") {
             setUser(response.data.user);
             console.log(user)
+
+            fetchUserRestaurants(username);
             // setEditedName(response.data.name);
           } else {
             setUserError('Failed to fetch user data');
@@ -35,7 +39,25 @@ const Profile = () => {
       }
     };
 
-    fetchUser();
+    const fetchUserRestaurants = async (username) => {
+      try {
+        console.log('Fetching user history for username:', username); // Debugging log
+        const restaurantsResponse = await axios.get(`/api/userInfo/userHistory?username=${username}`);
+        console.log('User history response:', restaurantsResponse.data); // Debugging log
+
+        if (restaurantsResponse.data.status === 'success') {
+          setRestaurants(restaurantsResponse.data.restaurants);
+        } else {
+          setUserError('Failed to fetch restaurants data');
+        }
+      } catch (error) {
+        console.error('Failed to fetch user restaurants', error);
+        setUserError('An error occurred while fetching restaurants data');
+      }
+    };
+
+    fetchUserData();
+
   }, []);
 
   // const handleNameChange = (event) => {
@@ -70,6 +92,11 @@ const Profile = () => {
           {/* <button onClick={handleEdit}>Edit Profile</button> */}
           <div>
             <h1>Restaurants Visited</h1>
+            <div>
+              {restaurants.map((restaurant) => (
+                <RestaurantCard key={restaurant._id} restaurant={restaurant} />
+              ))}
+            </div>
           </div>
         </div>
       ) : (
